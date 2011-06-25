d0e774f26d1eea46f97dd5c5f1dfd0073e632504
Howard Yeh <hayeah@gmail.com>
2011-06-25 15:46:29 +0800

    serialize walls to array

--- map-builder/public/js/client.coffee
+++ map-builder/public/js/client.coffee
@@ -10,25 +10,42 @@ class MapBuilder
     @width = w
     @height = h
     @create_grid()
+  walls: () ->
+    walls = []
+    for row in @rows
+      for box in row
+        if box.is_wall()
+          walls.push(box.position())
+    walls
+  to_json: () ->
+    {walls: @walls()}
   create_grid: () ->
+    @rows = []
     i = 0
     while(i < @height)
       row = $("<div>")
       @e.append(row)
+      boxes = []
+      @rows.push(boxes)
       j = 0
       while(j < @width)
-        box = $("<div class='box'>")
-        row.append(box)
-        new Box(box,j,i)
+        div = $("<div class='box'>")
+        row.append(div)
+        box = new Box(div,j,i)
+        boxes.push(box)
         j += 1
       i += 1
 
 class Box
   constructor: (box,x,y) ->
     @box = box
-    @box.data("x",x)
-    @box.data("y",y)
+    @x = x
+    @y = y
     @box.bind "click", () =>
       @box.toggleClass("wall")
+  position: () ->
+    [@x,@y]
+  is_wall: () ->
+    @box.hasClass("wall")
 
 
--- map-builder/public/js/client.js
+++ map-builder/public/js/client.js
@@ -12,18 +12,42 @@
       this.height = h;
       this.create_grid();
     }
+    MapBuilder.prototype.walls = function() {
+      var box, row, walls, _i, _j, _len, _len2, _ref;
+      walls = [];
+      _ref = this.rows;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        row = _ref[_i];
+        for (_j = 0, _len2 = row.length; _j < _len2; _j++) {
+          box = row[_j];
+          if (box.is_wall()) {
+            walls.push(box.position());
+          }
+        }
+      }
+      return walls;
+    };
+    MapBuilder.prototype.to_json = function() {
+      return {
+        walls: this.walls()
+      };
+    };
     MapBuilder.prototype.create_grid = function() {
-      var box, i, j, row, _results;
+      var box, boxes, div, i, j, row, _results;
+      this.rows = [];
       i = 0;
       _results = [];
       while (i < this.height) {
         row = $("<div>");
         this.e.append(row);
+        boxes = [];
+        this.rows.push(boxes);
         j = 0;
         while (j < this.width) {
-          box = $("<div class='box'>");
-          row.append(box);
-          new Box(box, j, i);
+          div = $("<div class='box'>");
+          row.append(div);
+          box = new Box(div, j, i);
+          boxes.push(box);
           j += 1;
         }
         _results.push(i += 1);
@@ -35,12 +59,18 @@
   Box = (function() {
     function Box(box, x, y) {
       this.box = box;
-      this.box.data("x", x);
-      this.box.data("y", y);
+      this.x = x;
+      this.y = y;
       this.box.bind("click", __bind(function() {
         return this.box.toggleClass("wall");
       }, this));
     }
+    Box.prototype.position = function() {
+      return [this.x, this.y];
+    };
+    Box.prototype.is_wall = function() {
+      return this.box.hasClass("wall");
+    };
     return Box;
   })();
 }).call(this);
