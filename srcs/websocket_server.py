5054532ea4094335af5bec929047244ead1eb4e4
linjunhalida <linjunhalida@gmail.com>
2011-06-30 21:46:53 +0800

    hack zmq for tornado

--- srcs/websocket_server.py
+++ srcs/websocket_server.py
@@ -4,42 +4,45 @@
 module: server
 """
 import sys, os, time, logging
-import tornado.ioloop, tornado.web, tornado.websocket
+
+import tornado
+import tornado.web, tornado.websocket
 
 import zmq
+from zmq.eventloop import ioloop, zmqstream
+
+# hack
+tornado.ioloop = ioloop
+
 context = zmq.Context()
+
 # 用来接受info更新
 suber = context.socket(zmq.SUB)
 suber.connect('ipc:///tmp/game_puber.ipc')
 # self.suber.setsockopt(zmq.SUBSCRIBE, 'room:%d '%room)
 suber.setsockopt(zmq.SUBSCRIBE, '')
+
+
 # 用来与服务器交互
 oper = context.socket(zmq.REQ)
 oper.connect('ipc:///tmp/game_oper.ipc')
 
 
 class InfoWebSocket(tornado.websocket.WebSocketHandler):
+    
     connects = []
     room = -1
 
     @classmethod
-    def check_info(cls):
-        # 计划好下次更新
-        cls.io_loop.add_timeout(time.time()+0.5,
-                                InfoWebSocket.check_info)
-        while True:
-            # 检查是否有更新
-            try:
-                data = suber.recv(zmq.NOBLOCK)
-                # 拆分掉room头信息
-                i = data.index(' ')
-                room = int(data[:i].split(':')[1])
-                # print "on sub info: ", data
-                # 发送给所有注册到这个room的连接
-                cls.send_info(room, data[i:])
-            except zmq.ZMQError:
-                return
-            
+    def check_info(cls, data):
+        # 拆分掉room头信息
+        data = data[0]
+        i = data.index(' ')
+        room = int(data[:i].split(':')[1])
+        # print "on sub info: ", data
+        # 发送给所有注册到这个room的连接
+        cls.send_info(room, data[i:])
+
     @classmethod
     def send_info(cls, room, info):
         for c in cls.connects:
@@ -50,7 +53,7 @@ class InfoWebSocket(tornado.websocket.WebSocketHandler):
         self.connects.append(self)
         
     def on_message(self, message):
-        print message
+        # print message
         if message.startswith('room:'):
             self.room = int(message[5:])
         else:
@@ -59,11 +62,16 @@ class InfoWebSocket(tornado.websocket.WebSocketHandler):
     def process_cmd(self, message):
         oper.send_unicode(message)
         result = oper.recv()
-        print result
+        # print result
         self.write_message(result)
 
     def on_close(self):
         self.connects.remove(self)
+
+
+stream = zmqstream.ZMQStream(suber, tornado.ioloop.IOLoop.instance())
+stream.on_recv(InfoWebSocket.check_info)
+        
 
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
@@ -83,9 +91,6 @@ def main():
         io_loop = tornado.ioloop.IOLoop.instance()
 
         InfoWebSocket.io_loop = io_loop
-        # 每隔若干时间, 检查有没有pub
-        io_loop.add_timeout(time.time()+1, InfoWebSocket.check_info)
-        
         io_loop.start()
     except KeyboardInterrupt:
         print "bye!"
